e4b6108270f89143cb9be69884f25fa2d2ce1734
Carl Sebastian Malm <crozz17@hotmail.com>
2014-01-27 20:23:37 +0100

    Gallery v0.1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php $title='Strings'; include ('header.php'); ?>
+
+
+<h1><u>Gallery</u></h1>
+<div class='gallery'>
+  <div class='gallery-current'><img/></div>
+  <div class='gallery-all'>
+  <img src1='img/aik-tifo1.jpg'/>
+  <img src1='img/aik-tifo2.jpg'/>
+  <img src1='img/aik-tifo3.jpg'/>
+  <img src1='img/aik-tifo4.jpg'/>
+  <img src1='img/aik-tifo5.jpg'/>
+  <img src1='img/aik-tifo6.jpg'/>
+  </div>
+</div> <!-- end of gallery -->
+<p>An image gallery may look in many ways but it usually acts as a container for a bunch om images and allows for the user to easily traverse and look at the images.</p>
+ 
+<p>Download this plugin <a href="http://github.com/csm93/gallery">here</a>.</p>
+
+ 
+<?php include ('footer.php'); ?>
