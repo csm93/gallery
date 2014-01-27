f24d9e351bb938aa75989dd84512d662b5fed17a
Carl Sebastian Malm <crozz17@hotmail.com>
2014-01-27 20:26:41 +0100

    Gallery v0.1.1

--- index.php
+++ index.php
@@ -5,16 +5,18 @@
 <div class='gallery'>
   <div class='gallery-current'><img/></div>
   <div class='gallery-all'>
-  <img src1='img/aik-tifo1.jpg'/>
-  <img src1='img/aik-tifo2.jpg'/>
-  <img src1='img/aik-tifo3.jpg'/>
-  <img src1='img/aik-tifo4.jpg'/>
-  <img src1='img/aik-tifo5.jpg'/>
-  <img src1='img/aik-tifo6.jpg'/>
+  <img src='add image here.jpg'/>
+  <img src='add image here.jpg'/>
+  <img src='add image here.jpg'/>
+  <img src='add image here.jpg'/>
+  <img src='add image here.jpg'/>
+  <img src='add image here.jpg'/>
   </div>
 </div> <!-- end of gallery -->
 <p>An image gallery may look in many ways but it usually acts as a container for a bunch om images and allows for the user to easily traverse and look at the images.</p>
- 
+
+<p>Just download this plugin for free and add your images in 'index.php'!</p>  
+  
 <p>Download this plugin <a href="http://github.com/csm93/gallery">here</a>.</p>
 
  
