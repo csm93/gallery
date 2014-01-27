6ffe1ad1175cb00c48359bcd1057e980a5a51427
Carl Sebastian Malm <crozz17@hotmail.com>
2014-01-27 20:31:13 +0100

    Gallery v0.1.3

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
-<?php $title='Strings'; include ('header.php'); ?>
+<?php $title='Gallery'; include ('header.php'); ?>
 
 
 <h1><u>Gallery</u></h1>
 <div class='gallery'>
-  <div class='gallery-current'><img/></div>
-  <div class='gallery-all'>
-  <img src='add image here.jpg'/>
-  <img src='add image here.jpg'/>
-  <img src='add image here.jpg'/>
-  <img src='add image here.jpg'/>
-  <img src='add image here.jpg'/>
-  <img src='add image here.jpg'/>
-  </div>
+<div class='gallery-current'><img/></div>
+<div class='gallery-all'>
+<img src='add image here.jpg'/>
+<img src='add image here.jpg'/>
+<img src='add image here.jpg'/>
+<img src='add image here.jpg'/>
+<img src='add image here.jpg'/>
+<img src='add image here.jpg'/>
+</div>
 </div> <!-- end of gallery -->
 <p>An image gallery may look in many ways but it usually acts as a container for a bunch om images and allows for the user to easily traverse and look at the images.</p>
 
-<p>Just download this plugin for free and add your images in 'index.php'!</p>  
-  
+<p>Just download this plugin for free and add your images in 'index.php'!</p>
 <p>Download this plugin <a href="http://github.com/csm93/gallery">here</a>.</p>
 
- 
 <?php include ('footer.php'); ?>
